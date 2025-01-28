32075b30d16c4b607e19b490ad595c396b5e3d17
azyMD <dgodorog@gmail.com>
2025-01-28 21:08:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get('/auth', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
